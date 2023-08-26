821599560c97599ac1e41d00a22a077f1cf1ec32
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-08-26 15:22:02 -0500

    test

---
+++ giants-relay/src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Star Players</li>
+          <li>Team</li>
+          <li>News</li>
+        </ul>
+      </nav>
+      <h1>Welcome to all things New York Giants - Giants Wire</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ giants-relay/src/footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function footer() {
+  return <footer>Giants Wire - A blog by Bilal Shahzad</footer>;
+}
+
+export default footer;
