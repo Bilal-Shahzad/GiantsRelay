171b901dffb19c8ecf6738d789b41910a4e6b85b
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-08-31 00:22:30 -0500

    fixes to header

--- giants-relay/src/App.js
+++ giants-relay/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header/Header';
+import Body from './Body/Body';
 import StarPlayersPage from './Schedule/StarPlayersComponent';
 import TeamPage from './Team/TeamComponent';
 import NewsPage from './News/NewsComponent';
-import Body from './Body/Body'
 import Footer from './Footer/footer';
 
 function App() {
@@ -14,11 +14,11 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
+          <Route path="/" element={<Body />} />
           <Route path="/star-players" element={<StarPlayersPage />} />
           <Route path="/team" element={<TeamPage />} />
           <Route path="/news" element={<NewsPage />} />
         </Routes>
-        <Body />
         <Footer />
       </div>
     </Router>
--- giants-relay/src/Header/Header.js
+++ giants-relay/src/Header/Header.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom'; // Import Outlet
 
 function Header() {
   return (
-    <header>
+    <><header>
       <nav>
         <ul>
-          <li><NavLink to="/star-players">Star Players</NavLink></li>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/star-players">Schedule</NavLink></li>
           <li><NavLink to="/team">Team</NavLink></li>
           <li><NavLink to="/news">News</NavLink></li>
         </ul>
       </nav>
       <h1>Welcome to all things New York Giants - Giants Wire</h1>
-    </header>
+    </header><Outlet /></> 
   );
 }
 
