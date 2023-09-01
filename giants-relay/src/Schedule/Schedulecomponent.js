18b8efa6e7f5293d74c8fa620a449861d0eb61a2
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-08-31 21:16:20 -0500

    fixing schedule

--- giants-relay/src/Schedule/Schedulecomponent.js
+++ giants-relay/src/Schedule/Schedulecomponent.js
@@ -1,54 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 function ScheduleComponent() {
-  const [teamInfo, setTeamInfo] = useState(null);
-
-  useEffect(() => {
-    const fetchTeamInfo = async () => {
-      const options = {
-        method: 'GET',
-        url: 'https://sportspage-feeds.p.rapidapi.com/teams',
-        params: {
-          league: 'NFL',
-          division: 'NFC EAST',
-          conference: 'NFC'
-        },
-        headers: {
-            'X-RapidAPI-Key': '',
-            'X-RapidAPI-Host': 'sportspage-feeds.p.rapidapi.com'
-        }
-      };
-
-      try {
-        const response = await axios.request(options);
-        if (response.data.results && response.data.results.length > 0) {
-          setTeamInfo(response.data.results[17]); 
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchTeamInfo();
-  }, []);
-
   return (
     <div>
-      <h2>Team Schedule</h2>
-      {teamInfo ? (
-        <div>
-          <p>Team: {teamInfo.team}</p>
-          <p>Mascot: {teamInfo.mascot}</p>
-          <p>Location: {teamInfo.location}</p>
-          <p>Conference: {teamInfo.conference}</p>
-          <p>Division: {teamInfo.division}</p>
-          <p>League: {teamInfo.league}</p>
-          <p>Abbreviation: {teamInfo.abbreviation}</p>
-        </div>
-      ) : (
-        <p>Loading team information...</p>
-      )}
+      <h2>Here Is The Schedule For The 2023 Season</h2>
     </div>
   );
 }
