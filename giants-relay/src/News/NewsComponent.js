c4d03a73303510dab72c85ef020a3febeffd09f0
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-09-11 23:43:03 -0500

    player stylig

--- giants-relay/src/News/NewsComponent.js
+++ giants-relay/src/News/NewsComponent.js
@@ -7,7 +7,7 @@ function NewsComponent() {
       date: 'Sun, 9/10',
       opponent: 'DAL',
       time: '7:20 PM',
-      recap: 'In the first game against the Dallas Cowboys.'
+      recap: 'In the first game against the Dallas Cowboys. The Giants had an abismal effort. They lost 40 to 0 to the Dallas Cowboys. In their defense, the first 16 points by the Cowboys were off of freak plays, a blocked kick returned for a touchdown, and a tipped pick 6. The offenseive line was abismal. Daniel Jones was pressured on 80% of his snaps. This offensive line made it impossible to play, the guards and RT were as awful as newborn trying to walk, it just did not work. The offense just was not producing and it hurt the team significantly. The defense did not do much, only one player was able to pressure Dak Prescott being Dexter Lawrence, the rest of the D-line was just underwhelming. Overall the team did not play, hopefuly they bounce back against the Arizona Cardinals next Sunday.'
     },
     {
       week: 2,
--- giants-relay/src/Team/TeamComponent.js
+++ giants-relay/src/Team/TeamComponent.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './TeamComponent.css'
 
 function TeamComponent() {
   const [teamData, setTeamData] = useState({});
@@ -35,21 +36,21 @@ function TeamComponent() {
     <div>
       <h2>Team: {teamData.team}</h2>
       <p>Here is the New York Giants roster for the 2023-2024 NFL Season:</p>
-      <ul>
-        {teamData.roster && teamData.roster.map((player, index) => (
-          <li key={index}>
-            <p>Name: {player.longName}</p>
-            <p>Position: {player.pos}</p>
-            <p>Jersey Number: {player.jerseyNum}</p>
-            <p>Age: {player.age}</p>
-            <p>Height: {player.height}</p>
-            <p>Weight: {player.weight} lbs</p>
-            <p>School: {player.school}</p>
-            <p>Experience: {player.exp} years</p>
-            <p> <img src={player.espnHeadshot} alt="Player Headshot" /></p>
-          </li>
-        ))}
-      </ul>
+      <ul className="player-list">
+      {teamData.roster && teamData.roster.map((player, index) => (
+      <li key={index} className="player-item">
+      <p className="player-detail"><strong>Name:</strong> {player.longName}</p>
+      <p className="player-detail"><strong>Position:</strong> {player.pos}</p>
+      <p className="player-detail"><strong>Jersey Number:</strong> {player.jerseyNum}</p>
+      <p className="player-detail"><strong>Age:</strong> {player.age}</p>
+      <p className="player-detail"><strong>Height:</strong> {player.height}</p>
+      <p className="player-detail"><strong>Weight:</strong> {player.weight} lbs</p>
+      <p className="player-detail"><strong>School:</strong> {player.school}</p>
+      <p className="player-detail"><strong>Experience:</strong> {player.exp} years</p>
+      <p className="player-detail"><strong></strong> <img src={player.espnHeadshot} alt="Player Headshot" /></p>
+    </li>
+  ))}
+</ul>
     </div>
   );
 }
