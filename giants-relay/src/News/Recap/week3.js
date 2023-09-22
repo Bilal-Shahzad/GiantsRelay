775d8ca38f185fe4ac07a8256b00c4666614d291
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-09-22 18:40:06 -0500

    updated text to week 3

--- giants-relay/src/News/Recap/week3.js
+++ giants-relay/src/News/Recap/week3.js
@@ -6,12 +6,20 @@ function Week3Recap() {
     <div className="week-recap"> 
       <h2>Week 2 Recap</h2>
       <p>
-        It was a solid effort by the Giants. They were expected to lose big to the 49ers but they held their own. 
-        It is hard to beat a team that is expected to go to the Super Bowl when you are missing your star player. 
-        Towards the end of the game it just unfolded for the Giants, their offense could not get going and their defense could not get off the field on 3rd down. 
-        Hopefully they are able to bounce back against the the Seahawks on MNF against the Seattle Seahawks. 
-      </p>
-    </div>
+      The Giants relentlessly blitzed Purdy, attempting it on 33 out of 39 dropbacks, as per NFL NextGen stats. Purdy responded effectively, gaining 247 of his 310 passing yards on those plays, including the touchdowns to rookie Ronnie Bell and Samuel.</p>
+
+<p>49ers RBMcCaffrey contributed 85 rushing yards, 34 receiving yards, and scored a 4-yard touchdown run, matching Jerry Rice's franchise record with touchdowns in 12 consecutive games in the regular season or playoffs, leaving him three short of the NFL record.
+The San Francisco defense performed admirably against the injury-plagued Giants, who struggled to move the ball and finished with a mere 150 yards of offense. The Giants managed to score two field goals in the first half and a touchdown run by Matt Breida in the third quarter, aided by penalties on the Niners.</p>
+
+<p>Giants' quarterback Daniel Jones faced constant pressure and struggled to establish a passing game, finishing with 137 yards, one interception, and a lack of offensive rhythm.
+Despite some initial inaccuracies, Purdy's numbers improved thanks to fortunate bounces and support from his teammates. Notably, Kittle prevented a potential interception, and a deflected pass to Samuel turned into a 15-yard gain when Ronnie Bell caught it.</p>
+
+<p>The Giants found themselves in a familiar situation, trailing by double digits at halftime, which has been a recurring issue in their recent games.
+A brief scuffle occurred late in the first half when the Niners were attempting to run out the clock. San Francisco left tackle Trent Williams and New York's A'Shawn Robinson were involved, but both received offsetting penalties, and Williams was not ejected.</p>
+
+<p>In terms of injuries and inactives, the Giants saw rookie CB Deonte Banks and DL D.J. Davidson leave the game with injuries.
+Next up, the Giants will host Seattle on Monday, October 2.</p>
+</div>
   );
 }
 
