d08bf9a5e3a30ab595fcaeb45c39589f47991e0a
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-11-06 20:55:49 -0600

    week 9 summary added

---
+++ giants-relay/src/News/Recap/week9.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './week.css'; 
+
+function Week9Recap() {
+  return (
+    <div className="week-recap"> 
+      <h2>Week 9 Recap</h2>
+      <p>
+      In the past two games, rookie Tommy DeVito has stepped in as the Giants' starting quarterback in the second quarter due to injuries to Tyrod Taylor and Daniel Jones. Taylor sustained a rib cage injury last week and was placed on injured reserve. Jones, who returned after missing three games with a neck injury, left the game with a knee injury in the second quarter, which will be assessed on Monday.
+</p>
+
+<p>
+DeVito had more opportunities to throw than in his previous appearance against the Jets, completing 15 of 20 passes for 175 yards, throwing a nine-yard touchdown to Wan'Dale Robinson, and two interceptions. Robinson's touchdown was his second career touchdown and the Giants' first fourth-quarter touchdown since September.
+</p>
+
+<p>
+DeVito threw his first career interception, ending the Giants' streak of 131 consecutive passes without an interception. Las Vegas capitalized on the Giants' two turnovers, scoring 10 points. The Giants' defense failed to record a sack for the fourth time this season and had no takeaways for the fifth time.
+</p>
+
+<p>
+Jones and DeVito were sacked eight times for a loss of 46 yards, marking the second-highest number of sacks allowed in Brian Daboll's tenure as head coach. Saquon Barkley led the team with 90 rushing yards and 23 receiving yards for 113 scrimmage yards.
+</p>
+
+<p>
+The Giants fell to 2-7 overall, 1-4 on the road, and 0-4 against the AFC. The Raiders, under interim coach Antonio Pierce, secured their first win, and the Giants are now 0-3 against teams with in-season coaching changes.
+</p>
+
+<p>
+The Giants trailed 24-0 at halftime, their second-largest halftime deficit of the season. The Raiders scored a touchdown on their first offensive possession, a feat accomplished by only one other opponent this season.
+</p>
+
+<p>
+Randy Bullock filled in for injured kicker Graham Gano and had two second-half touchback kickoffs. This was his second appearance with the Giants in his 10-season career.
+</p>
+
+<p>
+The Giants played without tight end Darren Waller, who is on injured reserve with a hamstring injury. Tackles Andrew Thomas and Evan Neal returned after missing several games. The Giants fielded their eighth different offensive line combination in nine games.
+
+Cornerback Adoree' Jackson left the game with a concussion, while running back Deon Jackson and linebacker Justin Hollins made their Giants debuts. The Giants started defensive linemen Dexter Lawrence, Rakeem Nunez-Roches, and A'Shawn Robinson in their first game without Leonard Williams, who was traded last week. Safety Xavier McKinney led the team with 10 tackles.
+
+The Giants' inactive players included kicker Cade York, running back Jashaun Corbin, center Sean Harlow, tackle Joshua Miles, safety Gervarrius Owens, and nose tackle Jordon Riley.</p>
+
+<p>
+In conclusion, the Giants faced a challenging game marked by quarterback injuries, offensive struggles, and defensive lapses. The promising performance of rookie Tommy DeVito and the return of key players like Saquon Barkley and the offensive line provided some positive aspects, but turnovers and a lack of sacks and takeaways continued to plague the team. The Giants now have a tough road ahead, striving to overcome adversity and improve their record in a season marked by ups and downs.
+</p>
+</div>
+  );
+}
+
+export default Week9Recap;
