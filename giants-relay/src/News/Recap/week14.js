8b933a0fc22d76cba5d1e4492c50a611e088b534
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-12-17 23:02:25 -0600

    week 14 & 15 pushed up

---
+++ giants-relay/src/News/Recap/week14.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './week.css'; 
+
+function Week14Recap() {
+  return (
+    <div className="week-recap"> 
+      <h2>Week 14 Recap</h2>
+      <p>
+      The New York Giants delivered a thrilling episode of The Tommy DeVito Show, securing a 24-22 victory over the Green Bay Packers and reigniting their playoff hopes. The game concluded with Randy Bullock making a 37-yard field goal as time expired, propelling the Giants to a 5-8 record and placing them one game behind the final playoff spot in the NFC. Despite the Packers retaking the lead with 1:33 remaining, they fell to 6-7.
+</p>
+
+<p>
+Looking ahead, the Giants face back-to-back road trips to New Orleans and Philadelphia. The Saints, with a 6-7 record, are coming off a 28-6 victory over the Panthers, while the Eagles, now 10-3, aim to rebound after consecutive losses to the 49ers and Cowboys.
+</p>
+
+<p>
+In the offensive showcase, undrafted rookie Tommy DeVito continued to shine, starting for the fourth consecutive week. Coach Brian Daboll's creativity was evident in the game, with Saquon Barkley lining up in the wildcat formation, resulting in a dynamic 32-yard run by Wan'Dale Robinson and a subsequent rushing touchdown by Barkley. DeVito showcased both his passing and running abilities, completing 17 of 21 passes for 158 yards and contributing 71 rushing yards on 10 carries.
+</p>
+
+<p>
+In the offensive showcase, undrafted rookie Tommy DeVito continued to shine, starting for the fourth consecutive week. Coach Brian Daboll's creativity was evident in the game, with Saquon Barkley lining up in the wildcat formation, resulting in a dynamic 32-yard run by Wan'Dale Robinson and a subsequent rushing touchdown by Barkley. DeVito showcased both his passing and running abilities, completing 17 of 21 passes for 158 yards and contributing 71 rushing yards on 10 carries.
+</p>
+
+<p>
+Despite Barkley surpassing 5,000 career rushing yards and the Giants scoring touchdowns on their first three trips to the red zone, the game took a dramatic turn when Barkley fumbled in the fourth quarter. The Packers capitalized on the turnover to take the lead, but the Giants rallied in the final moments, securing the victory with Bullock's game-winning field goal.
+</p>
+
+<p>
+The Giants now look forward to their upcoming challenges, aiming to build on the momentum from this thrilling victory as they pursue their playoff aspirations.
+</p>
+</div>
+  );
+}
+
+export default Week14Recap;
---
+++ giants-relay/src/News/Recap/week15.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './week.css'; 
+
+function Week15Recap() {
+  return (
+    <div className="week-recap"> 
+      <h2>Week 15 Recap</h2>
+      <p>
+      The New York Giants' three-game winning streak came to an end as they fell to the New Orleans Saints, bringing their record to 5-9 for the season. Despite the loss, the Giants remain mathematically alive in the NFC playoff race with three games remaining. Their next challenge awaits them on Christmas afternoon in Philadelphia.
+</p>
+
+<p>
+The defeat ensures that the Giants will finish the season with a sub-.500 record. The game featured an unusual sight as punter Jamie Gillan stepped in to kick a 40-yard field goal for the Giants' final points as time expired in the second quarter. This was Gillan's first field goal and the second attempt of his five-year career, with return specialist Gunner Olszewski serving as the emergency holder after Randy Bullock's injury on the kickoff following his 56-yard field goal in the first quarter.
+</p>
+
+<p>
+The Giants faced offensive challenges throughout the game, finishing with only 193 yards of total offense (60 rushing, 133 passing). Tommy DeVito, sacked seven times for a loss of 57 yards, encountered difficulties after a sack-free victory against Green Bay the previous week. The team converted only two third downs on 16 attempts, contributing to the offensive struggles.
+</p>
+
+<p>
+Despite the offensive hurdles, the game marked a notable defensive effort as neither team committed a turnover. This was the first time in a Giants game since their NFC Wild Card Game victory in Minnesota last January and the first time in the regular season since October 9, 2022, against Green Bay.
+</p>
+
+<p>
+Saquon Barkley, aiming for 1,000 yards from scrimmage for the fourth time in his career, fell short, rushing for a season-low 14 yards on nine carries and gaining 23 yards on two receptions. The Giants now look ahead to their next matchup in Philadelphia with the playoff race still within mathematical reach.
+</p>
+
+</div>
+  );
+}
+
+export default Week15Recap;
